34c4a7e6cb8715bea577b686a8d60d25a53a4a80
Park <parkyjuls@gmail.com>
2016-10-26 09:25:24 +0000

    마이그레이션 파일과 모델 파일 작성하고 회원 테이블 만들기

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026081616) do
+ActiveRecord::Schema.define(version: 20161026091845) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20161026081616) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
